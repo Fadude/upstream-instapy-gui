7db26d5768a8d0556d4b5d94c8db5bd29df79cdf
Asaf Fadida <fadida.asaf@gmail.com>
2020-08-30 14:49:29 +0000

    Added deploy script

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,5 +1,6 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push felixbreuer/instapy-config:latest
-docker push felixbreuer/instapy-auth:latest
-docker push felixbreuer/instapy-socket:latest
-docker push felixbreuer/instapy-client:latest
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#docker push fadude/instapy-config:latest
+#docker push fadude/instapy-auth:latest
+#docker push fadude/instapy-socket:latest
+#docker push fadude/instapy-client:latest
+docker push fadude/upstream-instapy-client:latest
