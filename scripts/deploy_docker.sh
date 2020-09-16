945cc425eb487a6b1f05cad604479d7e55924307
Asaf Fadida <fadida.asaf@gmail.com>
2020-09-16 23:22:03 +0300

    Added changed to travis integration

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,6 +1,7 @@
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker push fadude/instapy-config:latest
-#docker push fadude/instapy-auth:latest
-#docker push fadude/instapy-socket:latest
-#docker push fadude/instapy-client:latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push fadude/instapy-config:latest
+docker push fadude/instapy-auth:latest
+docker push fadude/instapy-socket:latest
+docker push fadude/instapy-client:latest
 docker push fadude/upstream-instapy-client:latest
+docker push fadude/upstream-instapy-ctrl:latest
