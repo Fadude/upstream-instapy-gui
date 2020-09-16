9bb66f6140daf3d99416e7bc7441aea754ab8bfe
Asaf Fadida <fadida.asaf@gmail.com>
2020-09-16 23:33:59 +0300

    Changed to needed images only

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,7 +1,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push fadude/instapy-config:latest
-docker push fadude/instapy-auth:latest
-docker push fadude/instapy-socket:latest
-docker push fadude/instapy-client:latest
-docker push fadude/upstream-instapy-client:latest
-docker push fadude/upstream-instapy-ctrl:latest
+#docker push fadude/instapy-config:latest
+#docker push fadude/instapy-auth:latest
+#docker push fadude/instapy-socket:latest
+#docker push fadude/instapy-client:latest
+docker push fadude/upstream-instapy-client-arm64:latest
+docker push fadude/upstream-instapy-ctrl-arm64:latest
